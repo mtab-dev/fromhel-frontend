4732cde5526ff846de4e09dd5a1f5f440e4ae134
Gabriel Araújo Lima <gabriel.araujo2902@outlook.com>
2024-06-15 15:04:33 -0300

    chore: implementing of subscribe form

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,15 +1,32 @@
-import { Component } from '@angular/core';
-import { ButtonComponent } from '../../components/ui/button/button.component';
+import { Component, ChangeDetectorRef } from '@angular/core'
+import { ButtonComponent } from '../../components/ui/button/button.component'
+import { InputComponent } from '../../components/ui/input/input.component'
+import { SubscribeComponent } from '../../components/layout/subscribe/subscribe.component'
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [ButtonComponent],
+  imports: [ButtonComponent, InputComponent, SubscribeComponent],
   templateUrl: './home.component.html',
-  styleUrl: './home.component.scss'
+  styleUrls: ['./home.component.scss']
 })
 export class HomeComponent {
+  showSubscribe: boolean = false
+
+  constructor(private cdr: ChangeDetectorRef) {
+    document.addEventListener('keydown', (e) => {
+      if (e.key.toLowerCase() !== 'escape') return
+      this.showSubscribe = false
+      this.cdr.detectChanges()
+    })
+  }
+
+  handleCancelSubscribe = () => {
+    this.showSubscribe = false
+    this.cdr.detectChanges()
+  }
+
   redirect(where: string): void {
-    location.href = where;
+    location.href = where
   }
 }
