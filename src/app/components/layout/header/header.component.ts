54aa0a1fee96f3e7025709cda641aa763a57b931
Gabriel Araújo Lima <gabriel.araujo2902@outlook.com>
2024-06-14 22:50:53 -0300

    feat: header component

---
+++ src/app/components/layout/header/header.component.ts
@@ -0,0 +1,16 @@
+import { Component, WritableSignal, signal } from '@angular/core';
+
+@Component({
+  selector: 'app-header',
+  standalone: true,
+  imports: [],
+  templateUrl: './header.component.html',
+  styleUrl: './header.component.scss'
+})
+export class HeaderComponent {
+  showMenu: WritableSignal<boolean> = signal<boolean>(false)
+
+  toggleMenu(): void {
+    this.showMenu.set(!this.showMenu())
+  }
+}
