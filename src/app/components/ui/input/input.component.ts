318915f75ce752b693ead29cc5c3f3437d362bcf
Gabriel Araújo Lima <gabriel.araujo2902@outlook.com>
2024-06-15 15:03:17 -0300

    feat: name, change methods to input component

--- src/app/components/ui/input/input.component.ts
+++ src/app/components/ui/input/input.component.ts
@@ -1,29 +1,25 @@
-import { Component, Input, WritableSignal, signal } from '@angular/core'
+import { Component, Input } from '@angular/core'
 
 @Component({
   selector: 'app-input',
   standalone: true,
-  imports: [],
   template: `
     <input
       [id]="id"
       [type]="type"
       [placeholder]="placeholder"
-      [value]="value()"
       class="inp"
-      (keyup)="handleValueChange($event)"
+      [name]="name"
+      (change)="keyUp($event)"
+      autocomplete="off"
     />
   `,
-  styleUrl: './input.component.scss'
+  styleUrls: ['./input.component.scss']
 })
 export class InputComponent {
   @Input() id: string = ''
   @Input() type: 'text' | 'email' | 'password' = 'text'
   @Input() placeholder: string = ''
-
-  value: WritableSignal<string> = signal<string>('')
-
-  handleValueChange(e: any): void {
-    this.value.set(e.target.value)
-  }
+  @Input() name: string = ''
+  @Input() keyUp: (e: any) => void = (e: any) => {}
 }
