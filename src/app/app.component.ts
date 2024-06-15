436229eca625565a3592748c8bc7b70fd45e758f
Gabriel Araújo Lima <gabriel.araujo2902@outlook.com>
2024-06-14 23:36:59 -0300

    chore: implementation of header and footer component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,16 @@
 import { Component } from '@angular/core'
 import { RouterOutlet } from '@angular/router'
+import { HeaderComponent } from './components/layout/header/header.component'
+import { FooterComponent } from './components/layout/footer/footer.component'
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HeaderComponent, FooterComponent],
   template: `
-  <router-outlet></router-outlet>
+    <app-header></app-header>
+    <router-outlet></router-outlet>
+    <app-footer></app-footer>
   `,
   styleUrl: './app.component.scss'
 })
