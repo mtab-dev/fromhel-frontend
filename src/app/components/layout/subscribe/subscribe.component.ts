2bb15d0eb886848b7fd5882f6a801b3efe2dd24b
Gabriel Araújo Lima <gabriel.araujo2902@outlook.com>
2024-06-17 23:46:46 -0300

    feat: requesting api to save user information

--- src/app/components/layout/subscribe/subscribe.component.ts
+++ src/app/components/layout/subscribe/subscribe.component.ts
@@ -1,6 +1,8 @@
 import { Component, Input } from '@angular/core'
 import { InputComponent } from '../../ui/input/input.component'
 import { ButtonComponent } from '../../ui/button/button.component'
+import { HttpClient } from '@angular/common/http'
+import { ENVIROMENTS } from '../../../config/env'
 
 @Component({
   selector: 'app-subscribe',
@@ -15,11 +17,33 @@ export class SubscribeComponent {
 
   @Input() handleCancelSubscribe: () => void = () => {}
 
-  handleNameChange(e: any): void {
-    this.name = e.target.value
+  constructor(private httpClient: HttpClient) {}
+
+  submit() {
+    const apiUrl = ENVIROMENTS.API_URL
+
+    if (!this.name || !this.email) return
+
+    this.httpClient
+      .post(`${apiUrl}clients/register`, {
+        clientName: this.name.toUpperCase(),
+        email: this.email.toLowerCase()
+      })
+      .subscribe(
+        (response) => {
+          console.log('Registro concluído com sucesso!', response)
+        },
+        (error) => {
+          console.error('Erro ao registrar cliente:', error)
+        }
+      )
   }
 
-  handleEmailChange(e: any): void {
-    this.email = e.target.value
+  handleNameChange(value: string): void {
+    this.name = value
+  }
+
+  handleEmailChange(value: string): void {
+    this.email = value
   }
 }
