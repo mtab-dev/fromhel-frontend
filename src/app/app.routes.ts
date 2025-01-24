e10b49a724cdfa473c8ff4d055ab8a18e9fb5979
matheus abrantes <abranteselcio@gmail.com>
2025-01-24 20:49:37 -0300

    feat: bulletspeel privacy polices

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,7 @@
 import { Routes } from '@angular/router'
 import { HomeComponent } from './pages/home/home.component'
 import { TeamComponent } from './pages/team/team.component'
+import { PrivacyPolicesComponent } from './privacy-polices/privacy-polices.component'
 
 export const routes: Routes = [
   {
@@ -12,6 +13,11 @@ export const routes: Routes = [
     path: 'team',
     component: TeamComponent,
     title: 'FromHel Studio | Equipe'
+  },
+  {
+    path: 'bulletspeel/privacy-polices',
+    component: PrivacyPolicesComponent,
+    title: 'FromHel Studio | Privacy Polices'
   },
   {
     path: '**',
---
+++ src/app/privacy-polices/privacy-polices.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-privacy-polices',
+  standalone: true,
+  imports: [],
+  templateUrl: './privacy-polices.component.html',
+  styleUrl: './privacy-polices.component.scss'
+})
+export class PrivacyPolicesComponent {
+
+}
